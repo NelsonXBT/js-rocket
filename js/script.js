cc81047416c893ea2c6476cf4124608ee5d7ff20
NelsonXBT Labs <104361802+NelsonXBT@users.noreply.github.com>
2025-06-10 17:58:45 +0100

    Made my rocket

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 
+            // This function does this:
+            var doCoolStuff = function () {
+                // Changing class name
+                var currentClassName = document.getElementById ('cool').className;
 
+                if (currentClassName == 'cool') {
+                    document.getElementById('cool').className = 'cool red';
+                } else {
+                    document.getElementById('cool').className = 'cool';
+                }
+            }
             
+
+
+
             var sayMyName = function (name) {
                 alert ('My name is:' +name);
             }
